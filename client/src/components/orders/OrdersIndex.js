87acc59dcc4aa5a2c5a8e8036eb2f60bf9ae195a
Bryan Lynch <bml312@nyu.edu>
2019-12-04 20:37:10 +0900

    gql nested json order type

--- client/src/components/orders/OrdersIndex.js
+++ client/src/components/orders/OrdersIndex.js
@@ -9,34 +9,47 @@ const OrdersIndex = (props) => (
             if (loading) return <div className="loader"></div>
             if (error) return `Error! ${error.message}`;
             const orders = data.orders;
-            return(
-                <div>
-                    <table>
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                {/* <th>Name</th>
-                                <th>Email</th> */}
-                                <th>Total</th>
-                                <th>View Full Order</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            { orders.map(order => 
-                                <tr key={order.id}>
-                                    <td>{order.id}</td>
-                                    {/* <td>{order.user.name}</td>
-                                    <td>{order.user.email}</td> */}
-                                    <td>{order.total}</td>
-                                    <td><Link to={"/staff/orders/" + order.id}>
-                                            View More Details
-                                        </Link>
-                                    </td>
-                                </tr>
-                            )}
-                        </tbody>
-                    </table>
-                </div>
+            return (
+              <div>
+                <table>
+                  <thead>
+                    <tr>
+                      <th>Customer Name</th>
+                      <th>Customer Email</th>
+                      <th>Subtotal</th>
+                      <th>Installation Fee</th>
+                      <th>Insurance Fee</th>
+                      <th>Total</th>
+                      <th>Confirmation #</th>
+                      <th>Order Date</th>
+                      <th>Insured</th>
+                      <th>Shipping Status</th>
+                      <th>View Full Order Details</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {orders.map(order => (
+                      <tr key={order.id}>
+                        <td>{order.billingInfo.name}</td>
+                        <td>{order.email}</td>
+                        <td>{order.subTotal}</td>
+                        <td>{order.installationFee}</td>
+                        <td>{order.insuranceFee}</td>
+                        <td>{order.total}</td>
+                        <td>{order.id}</td>
+                        <td>{order.date}</td>
+                        <td>{order.insured}</td>
+                        <td>{order.shippingInfo.shippingStatus}</td>
+                        <td>
+                          <Link to={"/staff/orders/" + order.id}>
+                            View More
+                          </Link>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
             );
         }}
     </Query>
--- server/schema/types/order_type.js
+++ server/schema/types/order_type.js
@@ -1,32 +1,64 @@
 const mongoose = require("mongoose");
 const graphql = require("graphql");
-const { GraphQLObjectType, GraphQLID, GraphQLInt, GraphQLList, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLID, GraphQLInt, GraphQLList, 
+    GraphQLString, GraphQLBoolean, GraphQL } = graphql;
 
 const Order = mongoose.model("order");
 
+const ShippingType = new GraphQLObjectType({
+  name: "ShippingType",
+  fields: {
+        shippingStatus: { type: GraphQLString },
+        name: { type: GraphQLString },
+        address1: { type: GraphQLString },
+        address2: { type: GraphQLString },
+        city: { type: GraphQLString },
+        state: { type: GraphQLString },
+        zipcode: { type: GraphQLString }
+    }
+});
+
+const BillingType = new GraphQLObjectType({
+  name: "BillingType",
+  fields: {
+    name: { type: GraphQLString },
+    address1: { type: GraphQLString },
+    address2: { type: GraphQLString },
+    city: { type: GraphQLString },
+    state: { type: GraphQLString },
+    zipcode: { type: GraphQLString }
+  }
+});
+
 const OrderType = new GraphQLObjectType({
-    name: "OrderType",
-    fields: () => ({
-        id: { type: GraphQLID },
-        total: { type: GraphQLInt },
-        products: {
-            type: new GraphQLList(require("./product_type")),
-            resolve(parentValue) {
-                return Order.findById(parentValue.id)
-                    .populate("products")
-                    .then(order => order.products);
-            }
-        },
-        email: { type: GraphQLString },
-        user: {
-            type: require('./user_type'),
-            resolve(parentValue){
-                return Order.findById(parentValue.id)
-                    .populate("user")
-                    .then(order => order.user);
-            }
-        }
-    })
+  name: "OrderType",
+  fields: () => ({
+    id: { type: GraphQLID },
+    subtotal: { type: GraphQLInt },
+    installationFee: { type: GraphQLInt },
+    insured: { type: GraphQLBoolean },
+    insuranceFee: { type: GraphQLInt },
+    total: { type: GraphQLInt },
+    products: {
+      type: new GraphQLList(require("./product_type")),
+      resolve(parentValue) {
+        return Order.findById(parentValue.id)
+          .populate("products")
+          .then(order => order.products);
+      }
+    },
+    email: { type: GraphQLString },
+    user: {
+      type: require("./user_type"),
+      resolve(parentValue) {
+        return Order.findById(parentValue.id)
+          .populate("user")
+          .then(order => order.user);
+      }
+    },
+    billingInfo: { type: BillingType },
+    shippingInfo: { type: ShippingType }
+  })
 });
 
 module.exports = OrderType;
